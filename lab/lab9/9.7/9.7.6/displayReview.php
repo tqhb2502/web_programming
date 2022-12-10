4df2f9bb2365cb0e0e17d659ab261db4b111e9e9
tqhb2502 <84978747+tqhb2502@users.noreply.github.com>
2022-12-10 16:42:26 +0700

    lab9 completed

---
+++ lab/lab9/9.7/9.7.6/displayReview.php
@@ -0,0 +1,31 @@
+<?php
+    // set name of XML file
+    // normally this would come through GET
+    // it's hard-wired here for simplicity
+    $file = "57.xml";
+    // load file
+    $xml = simplexml_load_file($file) or die ("Unable to load XML file!");
+?>
+<html>
+    <head><basefont face="Arial"></head>
+    <body>
+        <!-- title and year -->
+        <h1><?php echo $xml->title; ?> (<?php echo $xml->year; ?>)</h1>
+        <!-- slug -->
+        <h3><?php echo $xml->teaser; ?></h3>
+        <!-- review body -->
+        <?php echo $xml->body; ?>
+        <br/>
+        <br/>
+        <!-- director, cast, duration and rating -->
+        <font size="2">
+            Director: <b><?php echo $xml->director; ?></b>
+            <br/>
+            Duration: <b><?php echo $xml->duration; ?> min</b>
+            <br/>
+            Cast: <b><ul><?php foreach ($xml->cast->person as $person) { echo "<li>$person</li>"; } ?></ul></b>
+            <br/>
+            Rating: <b><?php echo $xml->rating; ?></b>
+        </font>
+    </body>
+</html>
